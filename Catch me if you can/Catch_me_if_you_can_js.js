ecde886f2daeee821b98b2236eae6eb7c0fcc623
KamilSzydlowski <43911771+KamilSzydlowski@users.noreply.github.com>
2019-03-05 23:35:11 +0100

    0.2

--- Catch me if you can/Catch_me_if_you_can_js.js
+++ Catch me if you can/Catch_me_if_you_can_js.js
@@ -6,7 +6,7 @@ let websocket;
 let gracze = {};
 let nick = 'Dolan';
 let avek = "https://i.ibb.co/njBQ793/dolan-30-30.png";
-
+document.addEventListener('DOMContentLoaded', start);
 function initMap(){
     lokalizacja_start = {lat: -25.363, lng: 131.044};
     mapa = new google.maps.Map(document.getElementById('mapa'), {
@@ -19,7 +19,8 @@ function initMap(){
         position: lokalizacja_start,
         map: mapa,
         animation: google.maps.Animation.DROP,
-        icon: avek
+        icon: avek,
+        title: "Dolan!"
         
     });
     pobierzLokalizacje();
@@ -27,6 +28,12 @@ function initMap(){
     window.addEventListener('keydown', poruszMarkerem);
     //
     //
+}
+function start(){
+    let wyslij = document.getElementById("wyslij");
+    wyslij.addEventListener('click', WyslijWiadomosc);
+    let ok = document.getElementById('button_ok');
+    ok.addEventListener('click', WprowadzLogin);
 }
 function poruszMarkerem(e){
     let lat = marker.getPosition().lat();
@@ -82,6 +89,11 @@ function WebSocket_MESSAGE(e) {
             lng: data.lng
         });
     }
+    if(data.includes("msg")) {
+        //utw div
+        //uzup div nowa_wiadomosc
+        //dod do okna
+    }
 }
 function loc_Zezwalaj(data) {
     let coords = {
@@ -96,4 +108,19 @@ function loc_Blokuj (err) {
 }
 function pobierzLokalizacje(){
     navigator.geolocation.getCurrentPosition(loc_Zezwalaj, loc_Blokuj);
+}
+function WyslijWiadomosc(){
+    let wiadomosc = document.getElementById("wpisz_tekst").value;
+    let wyslij = {
+        msg: wiadomosc,
+        nick: nick
+    };
+    websocket.send(JSON.stringify(wyslij));
+    wiadomosc.value = '';
+}
+function WprowadzLogin() {
+    nick = document.getElementById('wpisz_login').value;
+    let okno_login = document.querySelector(".okno_login");
+    okno_login.classList.add('schowaj');
+    document.getElementById('info').innerHTML = `Twój login to <b>${nick}</b>`;
 }
