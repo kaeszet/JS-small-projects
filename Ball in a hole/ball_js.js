4f95eb4f2194a30d0b4605d14d8a93b96c84554b
KamilSzydlowski <43911771+KamilSzydlowski@users.noreply.github.com>
2019-03-15 00:23:38 +0100

    0.75

--- Ball in a hole/ball_js.js
+++ Ball in a hole/ball_js.js
@@ -9,10 +9,12 @@ let ctx_ball = canvas_ball.getContext ('2d');
 let output = document.querySelector ('.output');
 let maxX = canvas.width - canvas_ball.width; //poll.clientWidth - ball.clientWidth;
 let maxY = canvas.height - canvas_ball.height; //poll.clientHeight - ball.clientHeight;
-const start_value = 20;
-let iloscDziurek = 0;
+let score = parseInt(localStorage.getItem(`score`), 10) || 0;
+let iloscDziurek = score + 1;
 let score_f = document.querySelector ('#score');
-let score = 0;
+score_f.innerHTML = score;
+let dziurkaArray = [];
+
 //document.addEventListener('DOMContentLoaded', uruchom)
 function uruchom () {
   ctx.beginPath ();
@@ -22,8 +24,12 @@ function uruchom () {
   stworzPilke (ctx_ball, canvas_ball, 10, 'black');
   stworzDziurke ();
   stworzCel();
-  ctx.drawImage (canvas_ball, start_value, start_value);
+  ctx.drawImage (canvas_ball, canvas.width / 2 - canvas_ball.width / 2, canvas.height / 2 - canvas_ball.height / 2);
   window.addEventListener ('deviceorientation', ruszaj);
+  document.getElementById('reset').addEventListener('click', reset);
+  window.addEventListener('beforeunload', function(event) {
+    localStorage.clear();
+ }, false);
 }
 function stworzPilke (context, canv, promien, kolor) {
   context.beginPath ();
@@ -38,7 +44,7 @@ function stworzPilke (context, canv, promien, kolor) {
   context.stroke ();
   context.fillStyle = `${kolor}`;
   context.fill ();
-  context.lineWidth = 5;
+  context.lineWidth = 1;
   context.strokeStyle = `#396b2b`;
   context.stroke ();
   //context.drawImage(canvas, 20, 20);
@@ -51,8 +57,8 @@ function ruszaj (e) {
   let old_top = canvas_ball.top;
   let old_left = canvas_ball.left;
   if (old_top == undefined) {
-    old_top = start_value;
-    old_left = start_value;
+    old_top = canvas.height / 2 - canvas_ball.height / 2;
+    old_left = canvas.width / 2 - canvas_ball.width / 2;
   }
   ctx.drawImage (canvas_ball, old_left, old_top);
   //ctx.clearRect()
@@ -77,25 +83,46 @@ function ruszaj (e) {
   ctx_ball.strokeStyle = `#396b2b`;
   ctx_ball.stroke ();
   ctx.drawImage (canvas_ball, canvas_ball.left, canvas_ball.top);
-  if (
-    canvas_ball.left >= (canvas.width / 2) - (cel.width / 2) &&
-    canvas_ball.left <= (canvas.width / 2) - (cel.width / 2) + cel.width - canvas_ball.width &&
-    canvas_ball.top >= cel.top &&
-    canvas_ball.top <= cel.height - (cel.height / 2)
-
-    //canvas_ball.left >= dziurka.left &&
-    //canvas_ball.left <= dziurka.left + 25 &&
-    //canvas_ball.top >= dziurka.top &&
-    //canvas_ball.top <= dziurka.top + 25
-  ) {
-    score++;
-    
-    alert ('Trafiony');
-    score_f.innerHTML = score;
-    stworzDziurke ();
-    stworzCel();
-    
+  if (score % 2 == 0) {
+    if (
+      canvas_ball.left >= (canvas.width / 2) - (cel.width / 2) - canvas_ball.width + ctx_ball.lineWidth &&
+      canvas_ball.left <= (canvas.width / 2) - (cel.width / 2) + cel.width - ctx_ball.lineWidth &&
+      canvas_ball.top >= cel.height - cel.height &&
+      canvas_ball.top <= cel.height - (cel.height / 2)
+  
+      //canvas_ball.left >= dziurka.left &&
+      //canvas_ball.left <= dziurka.left + 25 &&
+      //canvas_ball.top >= dziurka.top &&
+      //canvas_ball.top <= dziurka.top + 25
+    ) {
+      punkt();
+    }
   }
+  else {
+    if (
+      canvas_ball.left >= (canvas.width / 2) - (cel.width / 2) - canvas_ball.width + ctx_ball.lineWidth &&
+      canvas_ball.left <= (canvas.width / 2) - (cel.width / 2) + cel.width - ctx_ball.lineWidth &&
+      canvas_ball.top >= canvas.height - cel.height - canvas_ball.height &&
+      canvas_ball.top <= canvas.height
+  
+      //canvas_ball.left >= dziurka.left &&
+      //canvas_ball.left <= dziurka.left + 25 &&
+      //canvas_ball.top >= dziurka.top &&
+      //canvas_ball.top <= dziurka.top + 25
+    ) {
+      punkt();
+    }
+  }
+  dziurkaArray.forEach(dziurka => {
+    if (
+      canvas_ball.left >= dziurka.left - canvas_ball.width + ctx_ball.lineWidth &&
+      canvas_ball.left <= dziurka.left + dziurka.width - ctx_ball.lineWidth &&
+      canvas_ball.top >= dziurka.top - canvas_ball.height + ctx_ball.lineWidth &&
+      canvas_ball.top <= dziurka.top + dziurka.height - ctx_ball.lineWidth
+    ) {
+      przegrana("Przegrałeś!");
+    }
+  });
 }
 function stworzDziurke () {
   //z każdym punktem ma się tworzyć nowa zła dziurka min 20 od krawędzi, kolor czerwony
@@ -103,25 +130,29 @@ function stworzDziurke () {
 
   let czas;
   let ctx_dziurka;
-  czas = Date.now ();
-  canvas.innerHTML += `<canvas width="30" height="30" id="dziurka_${czas}"></canvas>`;
-  dziurka = document.getElementById (`dziurka_${czas}`);
-  ctx_dziurka = dziurka.getContext ('2d');
-  stworzPilke (ctx_dziurka, dziurka, 15, 'red');
-  let dziurkaHeight = canvas.height - dziurka.height; //poll.clientHeight - hole.clientHeight;
-  let dziurkaWidth = canvas.width - dziurka.width; //poll.clientWidth - hole.clientWidth;
-  let dziurkaX = Math.floor (Math.random () * dziurkaWidth) + 1;
-  let dziurkaY = Math.floor (Math.random () * dziurkaHeight) + 1;
-  dziurka.left = dziurkaX;
-  dziurka.top = dziurkaY;
-  ctx_dziurka.globalCompositeOperation = "xor";
+  for (i = 0; i < iloscDziurek; i++) {
+    czas = Date.now ();
+    canvas.innerHTML += `<canvas width="30" height="30" id="dziurka_${czas}"></canvas>`;
+    dziurka = document.getElementById (`dziurka_${czas}`);
+    ctx_dziurka = dziurka.getContext ('2d');
+    stworzPilke (ctx_dziurka, dziurka, 15, 'red');
+    let dziurkaHeight = canvas.height - dziurka.height; //poll.clientHeight - hole.clientHeight;
+    let dziurkaWidth = canvas.width - dziurka.width; //poll.clientWidth - hole.clientWidth;
+    let dziurkaX = Math.floor (Math.random () * dziurkaWidth) + 1;
+    let dziurkaY = Math.floor (Math.random () * dziurkaHeight) + 1;
+    dziurka.left = dziurkaX;
+    dziurka.top = dziurkaY;
+    dziurkaArray.push(dziurka);
+    ctx.drawImage(dziurka, dziurka.left, dziurka.top);
+  }
   iloscDziurek++;
-  ctx.drawImage(dziurka, dziurka.left, dziurka.top);
+  
   
 }
 function stworzCel() {
   let czas;
   let ctx_cel;
+  
   czas = Date.now ();
   canvas.innerHTML += `<canvas width="50" height="10" id="cel_${czas}"></canvas>`;
   cel = document.getElementById (`cel_${czas}`);
@@ -130,14 +161,67 @@ function stworzCel() {
   ctx_cel.rect (0, 0, cel.width, cel.height);
   ctx_cel.fillStyle = `#ffffff`;
   ctx_cel.fill ();
-  if (sciana == canvas.height - cel.height || sciana == undefined) {
+  if (score % 2 == 0) {
     ctx.drawImage(cel, (canvas.width / 2) - (cel.width / 2), 0);
   }
   else {
     ctx.drawImage(cel, (canvas.width / 2) - (cel.width / 2), canvas.height - cel.height);
   }
-  
-  
-  
 
 }
+function reset() {
+  localStorage.clear();
+  location.reload();
+}
+function punkt(){
+  score++;
+  score_f.innerHTML = score;
+  location.reload();
+  //doZapisu.push(score);
+  localStorage.setItem(`score`, `${score}`);
+}
+function przegrana(tekst) {
+  //alert('Przegrałeś!');
+  window.removeEventListener ('deviceorientation', ruszaj);
+  ctx.font = `30pt Arial`;
+  ctx.fillStyle = `pink`;
+  ctx.textAlign = `center`;
+  ctx.textBaseline = `middle`;
+  ctx.fillText(`${tekst}`, canvas.width / 2, canvas.height / 2);
+  ctx.font = `15pt Arial`;
+  ctx.fillText(`Twój wynik: ${score}`, canvas.width / 2, (canvas.height / 2) + 40 );
+  setTimeout(function() {reset();}, 2000);
+  
+}
+function startTimer(duration, display) {
+  let timer = duration, minutes, seconds;
+  let inter = 1000;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+      //timer--;
+      if (timer == 0) {
+        przegrana("Koniec czasu!");
+        timer = 0;
+        inter = 100000;
+        //alert("Koniec czasu. Zdobyłeś " + score + " punktów.");
+        //reset();
+      }
+      else {
+        timer--;
+      }
+      
+  }, inter);
+}
+
+setTimeout(function () {
+  const setCountdown = 30,
+      display = document.querySelector('#time');
+  startTimer(setCountdown, display);
+}, 20);
+
